ccb57f0cb6cd38bee89a00d0ad5c088cdf1578bb
taehoon <enter4652@naver.com>
2023-02-15 17:26:29 +0900

    web version

---
+++ app.py
@@ -0,0 +1,150 @@
+
+from flask import Flask, render_template, request, send_file, url_for, redirect, session
+import pymysql , os
+from tts_model import *
+from ai_human import *
+
+
+app = Flask(__name__)
+app.secret_key = b'_784DF99!87AF"QPWKERL$@[/'
+
+try:
+    db = pymysql.connect(host = "192.168.0.3",
+                        port = 3306,
+                        user = "btf",
+                        password = "Rlaxogns123!",
+                        charset = "utf8",
+                        db = "btf")
+except Exception as e:
+    print("DB ERROR")
+
+cur = db.cursor(pymysql.cursors.DictCursor)
+
+# -- 시작 페이지 (로그인) --
+@app.route("/")
+def signin():
+    session.clear()
+    return render_template('signin.html')
+
+# -- 로그인 페이지 --
+@app.route("/login", methods = ['POST'])
+def login():
+    Login_id = request.form['id']
+    Login_pw = request.form['pw']
+    
+    sql = f"SELECT * FROM user WHERE user_id = '{Login_id}' AND user_pw = '{Login_pw}'"
+    
+    cur.execute(sql)
+    select_login = cur.fetchone()
+    db.commit()
+    
+    print(Login_id, Login_pw, select_login)
+    session['id'] = request.form['id']
+    
+    if select_login == None:
+        return "0"
+    elif select_login['user_id'] == Login_id and select_login['user_pw'] == Login_pw:
+        return "1"
+
+    return render_template('signin.html')
+
+
+# -- 메인페이지 (TTS) --
+@app.route("/main")
+def index():
+    return render_template('index.html')
+
+
+# -- TTS 모델 작동 페이지 --
+@app.route("/down", methods=['GET', 'POST'])
+def tts():
+    if request.method == 'POST':
+        print("파일 개수 :", path_listdir)
+        
+        model = request.form['model']
+        textbox = request.form['textbox']
+        print(model, textbox)
+        
+        try:
+            path_dir = './TTSOUT/audio/'
+            path_listdir = os.listdir(path_dir)
+            [os.remove(f) for f in glob.glob('./TTSOUT/audio/*.wav')]  
+            
+            if len(path_listdir) == 0:
+                print('생성시작')
+                    
+                if model == "모델 선택":
+                    print("model error")
+                else:
+                    text_list = textbox.split('\r\n')
+                    print(type(text_list), text_list)
+                    text_edit(model, text_list)
+                    
+                    return render_template('download.html')
+
+            else:
+                print('대본없음')
+                return redirect(url_for('index'))
+            
+        except:
+            print('except')
+            return redirect(url_for('index'))
+        
+    return redirect(url_for('index'))
+        
+
+# -- TTS 파일 다운로드 링크페이지 --
+@app.route("/downlink")
+def downlink():
+
+        print('file 다운시작')
+        return send_file('./TTSOUT/audio/voice.wav',
+                mimetype='audio/wav',
+                as_attachment=True,
+                attachment_filename="tts.wav")
+    
+
+# -- TTS + LipFake 페이지 --
+@app.route("/video", methods=['GET', 'POST'])
+def video():
+    return render_template('video.html')
+
+
+# -- TTS + LipFake 모델 작동 페이지 --
+@app.route("/fake", methods=['GET', 'POST'])
+def fake():
+    try:
+        [os.remove(f) for f in glob.glob('./static/video/*.mp4')]
+        [os.remove(f) for f in glob.glob('./static/audio/*.wav')]
+        [os.remove(f) for f in glob.glob('./static/lipfake/*.mp4')]
+        
+        video_file = request.files["video_file"]
+        audio_file = request.files["audio_file"]
+        
+        video_file.save("static/video/video.mp4")
+        audio_file.save("static/audio/audio.wav")
+        
+        video_file_path = "static/video/video.mp4"
+        audio_file_path = "static/audio/audio.wav"
+        
+        print(video_file, audio_file)
+        
+        start_tts(video_file_path, audio_file_path)
+        return render_template('fake_download.html')
+    except:
+        return redirect(url_for('video'))
+
+
+# -- TTS + LipFake 파일 다운로드 페이지 --
+@app.route("/fake_downlink")
+def fake_downlink():
+
+        print('file 다운시작')
+        return send_file('./static/lipfake/fake.mp4',
+                mimetype='video/mp4',
+                as_attachment=True,
+                attachment_filename="fake.mp4")
+        
+ 
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8888)
--- tts_model.py
+++ tts_model.py
@@ -1,17 +1,8 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 from threading import *
-from tkinter import *
-from tkinter import ttk, filedialog, messagebox
 import os, re, subprocess, glob
 from num2words import num2words as nws
 import soundfile as sf
 import numpy as np
-
-# -- 설정 --
-bg = '#2c3e50'
-bnbg = '#2f3640'
 
 
 # -- 숫자 -> 한글 변환 --
@@ -33,73 +24,11 @@ def num_kr_translator(number):
     return number
 
 
-# -- 텍스트 활성화 --
-def get_text_input(): # input_text 가져올 함수
-    result = tts_text_input_text.get("1.0","end")
-    print(result)# 텍스트 확인용
-    
-# -- 텍스트 불러오기 --
-def get_load_text():
-    try:
-        try:
-            file = filedialog.askopenfilename(initialdir='/', title='Select file', defaultextension='.txt',
-                                        filetypes=(("text files", "*.txt"),("all files", "*.*")))
-            f = open(file, 'rt', encoding='cp949')
-            tts_text_input_text.delete('1.0', END)
-            tts_text_input_text.insert(END,f.read())
-            loding_label.config(text = '텍스트 불러오기 성공')
-        except:
-            f = open(file, 'rt', encoding='utf-8')
-            tts_text_input_text.delete('1.0', END)
-            tts_text_input_text.insert(END,f.read())
-            loding_label.config(text = '텍스트 불러오기 성공')         
-    except:
-        loding_label.config(text = '텍스트 불러오기 에러')
-   
-
-# -- 텍스트 저장 --
-def save_text():
-    try:
-        file = filedialog.asksaveasfile(title= 'file save', mode='w', defaultextension=".txt",
-                                        filetypes=(("text files", "*.txt"),("all files", "*.*")))
-        if file != None:
-            lines = tts_text_input_text.get('1.0', 'end')
-            file.write(lines)
-            file.close()
-            loding_label.config(text = '텍스트 저장 성공')
-    except:
-        messagebox.showinfo('에러', '에러')
-
-
-# -- Thread --
-def therading_text_edit():
-    try:
-        therad = Thread(target = text_edit)
-        therad.daemon = True
-        therad.start()
-    except:
-        print(f'Thread error : {therad}')
-
-
-# -- TTS 모델 사용 --
-def text_edit():
+def text_edit(model, text):
     try:
         text_result_list = []
         tts_model_execution_list = []
-        
-        file = filedialog.askopenfilename(initialdir='/', title='Select file', defaultextension='.txt',
-                                    filetypes=(("text files", "*.txt"),("all files", "*.*")))
-        
-        try:
-            f = open(file, 'r', encoding='cp949')
-            txt_file = f.readlines()
-            print('cp949')
-        except:
-            f = open(file, 'r', encoding='utf-8')
-            txt_file = f.readlines()
-            print('utf-8')
-            
-        loding_label.config(text = '음성 생성 시작')
+        txt_file = text
 
         for i in range(len(txt_file)):
             sentence_break = (txt_file[i])
@@ -202,21 +131,19 @@ def text_edit():
         for i in text_result_list: # 숫자 -> 한글 변환 실행
             tts_model_execution_list.append(num_kr_translator(i))
         
-        
-        model_select = model_select_cbbox.get()
 
         # 단일화자
-        # if model_sel == 'AI_황이화':
+        # if model_sel == 'ai_h':
         #     models = 'model_1'
-        # elif model_sel == 'AI_추민선':
+        # elif model_sel == 'ai_c':
         #     models = 'model_2'
-
+        model_sel = model
         # 다중화자
-        if model_select == 'AI_황이화':
+        if model_sel == 'ai_h':
             models = '1'
-        elif model_select == 'AI_추민선':
+        elif model_sel == 'ai_c':
             models = '0'
-        print(model_select) # 선택한 모델 확인
+        print(model_sel) # 선택한 모델 확인
         
         tts_create_cnt = 1 # 진행률 표시용
         for i in tts_model_execution_list:
@@ -228,9 +155,6 @@ def text_edit():
             subprocess.run(cmd)
             
             print(f'{i} - 오디오 생성 진행중')
-            cnt = (tts_create_cnt/len(tts_model_execution_list))*100
-            loding_label.config(text = f'진행률 - {int(cnt)}%')         
-            tts_create_cnt += 1
             
         print('파일 이름 변경 시작')
         sample_path = glob.glob('./sample/**', recursive=True)
@@ -272,117 +196,23 @@ def text_edit():
             audio_list.append(cnt)
 
         audio_list = np.concatenate((audio_list), axis=0)
+
+        default_name = 'voice'
         
-        name = file_name_input_text.get('1.0', 'end-1c')
-        print(name)
-        
-        sf.write(f'./TTSOUT/audio/{name}.wav', audio_list, sample_rate)
+        sf.write(f'./TTSOUT/audio/{default_name}.wav', audio_list, sample_rate)
 
         # -- 생성된 파일 삭제 --
         [os.remove(f) for f in glob.glob('./TTSOUT/*.wav')]
         [os.remove(f) for f in glob.glob('./sample/*.wav')]
-        
         print("오디오 생성완료")
-        loding_label.config(text = '음성 생성 완료')
         
         # 다음 프로세스를 위해 초기화
         txt_file = ''
         tts_model_execution_list = []
-        f.close()
     except:
-        messagebox.showinfo('에러', '에러')
-        loding_label.config(text = '음성 생성')
+
         [os.remove(f) for f in glob.glob('./TTSOUT/*.wav')]
         [os.remove(f) for f in glob.glob('./sample/*.wav')]
-        f.close()
 
-    # ----------------------------------------------------------------------
-
-# -- tkinter tts program --
-def tk_tts_program(win, mainfont2, mainfont, k_font, text_font):
-    global loding_label, file_name_input_text, tts_text_input_text
-    
-    tts_program_window = Toplevel(win)
-    tts_program_window.geometry('800x630')
-    tts_program_window['bg'] = bg
-
-    text_fg = 'white'
-    btn_fg = 'white'
-
-    Label(tts_program_window, bg=bg, text='     ').pack(side='left') # 사각 패딩
-    Label(tts_program_window, bg=bg, text='     ').pack(side='right')
-    Label(tts_program_window, bg=bg, text='     ').pack(side='top')
-    Label(tts_program_window, bg=bg, text='     ').pack(side='bottom')
-
-    # -- 메인프레임 --
-    main_labelframe = LabelFrame(tts_program_window, bg=bg, text='Text To Speech', fg=text_fg, font=mainfont, pady=15)
-    main_labelframe.pack(fill='both', expand=True)
-
-
-    main_info_frame = Frame(main_labelframe, bg=text_fg, width=600, height=180)
-    main_info_frame.pack(expand=False, side='top')
-
-    file_info_labelframe = LabelFrame(main_info_frame, bg=bg, text='모델선택', fg=text_fg, font=mainfont2, width=300, height=180, pady=35, padx=30)
-    file_info_labelframe.pack(fill='both', expand=True, side='left')
-
-    text_info_labelframe = LabelFrame(main_info_frame, bg=bg, text='대본입력', fg=text_fg, font=mainfont2, width=400, height=300, pady=35, padx=50)
-    text_info_labelframe.pack(fill='both', expand=False, side='right')
-
-    Label(file_info_labelframe, text='모델선택   ', bg=bg, fg='white').grid(column=0, row=0)
-    Label(file_info_labelframe, text='파일이름   ', bg=bg, fg='white').grid(column=0, row=1)
-    
-    file_name_input_text = Text(file_info_labelframe, width=23, height=1, font=k_font)
-    file_name_input_text.insert(END , '파일 이름')
-    file_name_input_text.grid(column=2, row=1)
-    
-    global model_select_cbbox
-    model_select_cbbox = ttk.Combobox(file_info_labelframe)
-    model_select_cbbox['values'] = ('AI_황이화','AI_추민선', 'AI 3(준비중)', 'AI 4(준비중)')
-    model_select_cbbox.current(0)
-    model_select_cbbox.grid(column=2, row=0)
-    
-    # -- 참고사항 --
-    Label(file_info_labelframe, text='', bg=bg, fg=text_fg).grid(column=0, row=2) # 여백용
-    Label(file_info_labelframe, text=' * 사용 규칙 *', bg=bg, fg=text_fg, font=text_font).grid(columnspan=5, row=3)
-    Label(file_info_labelframe, text='1. 원하는 뉴스 대본 작성', bg=bg, fg=text_fg).grid(columnspan=5, row=4, sticky=W)
-    Label(file_info_labelframe, text='2. 대본 저장하기 버튼 클릭', bg=bg, fg=text_fg).grid(columnspan=5, row=5, sticky=W)
-    Label(file_info_labelframe, text='3. 대본 불러오기 버튼으로 대본 수정', bg=bg, fg=text_fg).grid(columnspan=5, row=6, sticky=W)
-    Label(file_info_labelframe, text='4. 2번 3번 반복', bg=bg, fg=text_fg).grid(columnspan=5, row=7, sticky=W)
-    Label(file_info_labelframe, text='5. 음성생성 버튼 클릭', bg=bg, fg=text_fg).grid(columnspan=5, row=8, sticky=W)
-    Label(file_info_labelframe, text='6. 파일 이름 기입 후 대본파일 선택 시 TTS 음성 생성', bg=bg, fg=text_fg).grid(columnspan=5, row=9, sticky=W)
-    Label(file_info_labelframe, text='', bg=bg, fg='white').grid(column=0, row=10)
-    Label(file_info_labelframe, text='*   비정상적 에러시 관리자 문의', bg=bg, fg='red').grid(columnspan=5, row=12, sticky=W)
-
-    tts_text_input_text = Text(text_info_labelframe)
-    tts_text_input_text.insert(END, '대본을 입력하세요.')
-    tts_text_input_text.place(width=300, height=260)
-
-
-    # -- 버튼 프레임 --
-    btn_frame = Frame(main_labelframe, bg=bg, width=695, height=50, pady=2)
-    btn_frame.pack(expand=True, side='bottom')
-
-    loding_label = Label(btn_frame, text='현재 상태가 표시됩니다.', bg=bg, fg=text_fg)
-    loding_label.grid(columnspan=10, row=0)
-
-    Label(btn_frame, bg=bg, text='     ').grid(column=1, row=1) # 버튼 공백용
-    load_textfile_btn = Button(btn_frame, bg=bnbg, text='대본 불러오기', fg=btn_fg, relief='flat', command=get_load_text, padx=30, pady=8)
-    load_textfile_btn.grid(column=0, row=2)
-
-    Label(btn_frame, bg=bg, text='     ').grid(column=1, row=2) # 버튼 공백용
-    save_textfile_btn = Button(btn_frame, bg=bnbg, text='대본 저장하기', fg=btn_fg, relief='flat', command=save_text, padx=30, pady=8)
-    save_textfile_btn.grid(column=2, row=2)
-
-    Label(btn_frame, bg=bg, text='     ').grid(column=3, row=2) # 버튼 공백용
-    tts_create_btn = Button(btn_frame, bg=bnbg, text='음성 생성', fg=btn_fg, relief='flat', command=therading_text_edit, padx=30, pady=8)
-    tts_create_btn.grid(column=4, row=2)
-
-    Label(btn_frame, bg=bg, text='     ').grid(column=5, row=2) # 버튼 공백용
-    gui_stop_btn = Button(btn_frame, bg=bnbg, text='강제 종료', fg=btn_fg, relief='flat', command=tts_program_window.destroy, padx=30, pady=8)
-    gui_stop_btn.grid(column=6, row=2)
-
-    [os.remove(f) for f in glob.glob('./TTSOUT/*.wav')]
-    [os.remove(f) for f in glob.glob('./sample/*.wav')]
-    
-    tts_program_window.resizable(False, False)
-    tts_program_window.mainloop()
+if __name__ == '__name__':
+    text_edit()
